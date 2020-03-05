9fe45979283795731ad56e8910ca426335f65921
Nick Jansen <n.jansen@sitepilot.io>
2020-03-05 05:36:40 -0800

    Update credits

--- resources/views/vendor/nova/partials/footer.blade.php
+++ resources/views/vendor/nova/partials/footer.blade.php
@@ -1,7 +1,7 @@
 <p class="mt-8 text-center text-xs text-80">
-    <a href="https://nova.laravel.com" class="text-primary dim no-underline">Laravel Nova</a>
+    <a href="https://github.com/autopilot/" class="text-primary dim no-underline">Autopilot</a>
     <span class="px-1">&middot;</span>
-    &copy; {{ date('Y') }} Laravel LLC - By Taylor Otwell, David Hemphill, and Steve Schoger.
+    &copy; {{ date('Y') }} Sitepilot - By Nick Jansen
     <span class="px-1">&middot;</span>
-    v{{ \Laravel\Nova\Nova::version() }}
+    Nova v{{ \Laravel\Nova\Nova::version() }}
 </p>
