4ed6771bdcddd5b6765bdd665161af3346b9391f
Nick Jansen <n.jansen@sitepilot.io>
2020-03-05 06:22:43 -0800

    Update copyright url

--- resources/views/vendor/nova/partials/footer.blade.php
+++ resources/views/vendor/nova/partials/footer.blade.php
@@ -1,5 +1,5 @@
 <p class="mt-8 text-center text-xs text-80">
-    <a href="https://github.com/autopilot/" class="text-primary dim no-underline">Autopilot</a>
+    <a href="https://github.com/sitepilot/autopilot/" target="_blank" class="text-primary dim no-underline">Autopilot</a>
     <span class="px-1">&middot;</span>
     &copy; {{ date('Y') }} Sitepilot - By Nick Jansen
     <span class="px-1">&middot;</span>
