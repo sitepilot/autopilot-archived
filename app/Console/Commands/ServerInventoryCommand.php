45fe59a4e691b48b2225466cf527e00d2c884c8d
Nick Jansen <n.jansen@sitepilot.io>
2020-03-06 03:56:32 -0800

    Fix invalid group name warning

--- app/Console/Commands/ServerInventoryCommand.php
+++ app/Console/Commands/ServerInventoryCommand.php
@@ -98,7 +98,7 @@ class ServerInventoryCommand extends Command
                 $meta['hostvars'][$host->name] = $hostvars;
             }
 
-            $output[Str::slug($group->name)] = $groupOutput;
+            $output[Str::snake($group->name)] = $groupOutput;
         }
 
         $output['_meta'] = $meta;
