1f3211c856a2927e9d29af38acbfc483f255bc2c
Nick Jansen <n.jansen@sitepilot.io>
2020-03-09 08:27:19 -0700

    Implement Nova responsiveness package

--- config/nova-theme-responsive.php
+++ config/nova-theme-responsive.php
@@ -18,7 +18,7 @@ return [
     'hide_update_and_continue_editing_button' => false,
 
     // If true, hides the "Update & Continue Editing" button on "Update" forms (mobile only)
-    'hide_update_and_continue_editing_button_on_mobile' => false,
+    'hide_update_and_continue_editing_button_on_mobile' => true,
 
     // If true, the sidebar will stay fixed on desktop view
     'fixed_sidebar' => false,
