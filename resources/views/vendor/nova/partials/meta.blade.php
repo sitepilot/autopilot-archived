bf82e0913f6db84cb8ce4ea7e3b4047d60cddc24
Nick Jansen <n.jansen@sitepilot.io>
2020-03-11 18:30:30 +0100

    Add app icon

--- resources/views/vendor/nova/partials/meta.blade.php
+++ resources/views/vendor/nova/partials/meta.blade.php
@@ -1,1 +1,2 @@
-{{-- <link rel="icon" type="image/png" href="{{ asset('/img/favicon.png') }} "> --}}
+<link rel="shortcut icon" type="image/x-icon" href="{{ asset('images/icon-dark.ico') }}" />
+<link rel="apple-touch-icon" href="{{ asset('images/icon.png') }}">
