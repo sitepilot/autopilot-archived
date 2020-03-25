3d7c25812e1b7f33847f048192faa4a0f8a35966
Nick Jansen <n.jansen@sitepilot.io>
2020-03-25 14:32:12 +0100

    Refactor ansible role (#28)
    * Refactor ansible role
    
    * Fix lint errors / warnings
    
    * Skip upgrade tag
    
    * Disable empty task include
    
    * Define python interpreter
    
    * Install docker when not autopilot host
    
    * Remove unused var

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -74,7 +74,7 @@ class DatabaseSeeder extends Seeder
                 'ansible_ssh_private_key_file' => '/var/www/html/vagrant/ssh/test_key',
                 'ansible_ssh_public_key_file' => '/var/www/html/vagrant/ssh/test_key.pub',
                 'ansible_ssh_common_args' => '-o StrictHostKeyChecking=no',
-                'autopilot_host' => env('APP_TEST_AUTOPILOT_HOST'),
+                'autopilot_host' => env('APP_TEST_AUTOPILOT_HOST')
             ];
             $host->save();
             $host->firewallRules()->attach([
