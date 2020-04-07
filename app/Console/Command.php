04d5301d9933deeda5ff8f368a34592c5d9ee6fb
Nick Jansen <n.jansen@sitepilot.io>
2020-04-07 14:03:11 +0200

    Add model parameters

--- app/Console/Command.php
+++ app/Console/Command.php
@@ -2,14 +2,22 @@
 
 namespace App\Console;
 
+use App\ServerApp;
 use App\ServerHost;
 use App\ServerUser;
+use App\ServerDatabase;
 use Illuminate\Console\Command as ConsoleCommand;
 
 class Command extends ConsoleCommand
 {
     protected $host = null;
     protected $user = null;
+    protected $app = null;
+    protected $database = null;
+    protected $hostModel = null;
+    protected $userModel = null;
+    protected $appModel = null;
+    protected $databaseModel = null;
     protected static $buffer = null;
 
     /**
@@ -20,6 +28,8 @@ class Command extends ConsoleCommand
     public function askHost()
     {
         if ($this->host = $this->option('host')) {
+            $this->hostModel = ServerHost::where('name', $this->host)->first();
+
             return;
         }
 
@@ -33,6 +43,7 @@ class Command extends ConsoleCommand
 
             if (count($options) > 0) {
                 $this->host = $this->choice('Select a host', $options);
+                $this->hostModel = ServerHost::where('name', $this->host)->first();
             }
         }
     }
@@ -46,11 +57,11 @@ class Command extends ConsoleCommand
     {
         if ($this->user = $this->option('user')) {
             $user = ServerUser::where('name', $this->option('user'))->first();
+            $this->userModel = ServerUser::where('name', $this->user)->first();
+
             if (isset($user->host->name)) {
                 $this->host = $user->host->name;
-            } elseif ($this->option('user') == 'all') {
-                $this->user = 'all';
-                $this->host = 'all';
+                $this->hostModel = ServerHost::where('name', $this->host)->first();
             }
 
             return;
@@ -58,8 +69,8 @@ class Command extends ConsoleCommand
 
         if (!$this->user) {
             $users = ServerUser::get();
-            $options = ['all'];
-            $hosts = ['all' => 'all'];
+            $options = [];
+            $hosts = [];
 
             foreach ($users as $user) {
                 $options[] = $user->name;
@@ -68,7 +79,103 @@ class Command extends ConsoleCommand
 
             if (count($options) > 0) {
                 $this->user = $this->choice('Select a user', $options);
+                $this->userModel = ServerUser::where('name', $this->user)->first();
                 $this->host = $hosts[$this->user];
+                $this->hostModel = ServerHost::where('name', $this->host)->first();
+            }
+        }
+    }
+
+    /**
+     * Ask for a app.
+     *
+     * @return void
+     */
+    public function askApp()
+    {
+        if ($this->app = $this->option('app')) {
+            $app = ServerApp::where('name', $this->option('app'))->first();
+            $this->appModel = ServerApp::where('name', $this->app)->first();
+
+            if (isset($app->user->name)) {
+                $this->user = $app->user->name;
+                $this->userModel = ServerUser::where('name', $this->user)->first();
+            }
+
+            if (isset($app->user->host->name)) {
+                $this->host = $app->user->host->name;
+                $this->hostModel = ServerHost::where('name', $this->host)->first();
+            }
+
+            return;
+        }
+
+        if (!$this->app) {
+            $apps = ServerApp::get();
+            $options = [];
+            $hosts = [];
+            $users = [];
+
+            foreach ($apps as $app) {
+                $options[] = $app->name;
+                $users[$app->name] = $app->user->name;
+                $hosts[$app->name] = $app->user->host->name;
+            }
+
+            if (count($options) > 0) {
+                $this->app = $this->choice('Select an app', $options);
+                $this->appModel = ServerApp::where('name', $this->app)->first();
+                $this->user = $users[$this->app];
+                $this->userModel = ServerUser::where('name', $this->user)->first();
+                $this->host = $hosts[$this->app];
+                $this->hostModel = ServerHost::where('name', $this->host)->first();
+            }
+        }
+    }
+
+    /**
+     * Ask for a database.
+     *
+     * @return void
+     */
+    public function askDatabase()
+    {
+        if ($this->database = $this->option('database')) {
+            $database = ServerDatabase::where('name', $this->option('database'))->first();
+            $this->databaseModel = ServerDatabase::where('name', $this->database)->first();
+
+            if (isset($database->user->name)) {
+                $this->user = $database->user->name;
+                $this->userModel = ServerUser::where('name', $this->user)->first();
+            }
+
+            if (isset($database->user->host->name)) {
+                $this->host = $database->user->host->name;
+                $this->hostModel = ServerHost::where('name', $this->host)->first();
+            }
+
+            return;
+        }
+
+        if (!$this->database) {
+            $databases = ServerDatabase::get();
+            $options = [];
+            $hosts = [];
+            $users = [];
+
+            foreach ($databases as $database) {
+                $options[] = $database->name;
+                $users[$database->name] = $database->user->name;
+                $hosts[$database->name] = $database->user->host->name;
+            }
+
+            if (count($options) > 0) {
+                $this->database = $this->choice('Select a database', $options);
+                $this->databaseModel = ServerDatabase::where('name', $this->database)->first();
+                $this->user = $users[$this->database];
+                $this->userModel = ServerUser::where('name', $this->user)->first();
+                $this->host = $hosts[$this->database];
+                $this->hostModel = ServerHost::where('name', $this->host)->first();
             }
         }
     }
