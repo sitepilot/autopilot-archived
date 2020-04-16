81856b500260eef8ec08d3dbf14a36c49af19b1b
Nick Jansen <n.jansen@sitepilot.io>
2020-04-16 11:53:40 +0200

    Update user test command

--- app/Console/Commands/UserTestCommand.php
+++ app/Console/Commands/UserTestCommand.php
@@ -2,12 +2,8 @@
 
 namespace App\Console\Commands;
 
-use Exception;
-use Laravel\Nova\Nova;
 use App\Console\Command;
-use App\Notifications\CommandFailed;
-use Symfony\Component\Process\Process;
-use Illuminate\Support\Facades\Notification;
+use App\Traits\HasState;
 
 class UserTestCommand extends Command
 {
@@ -18,7 +14,6 @@ class UserTestCommand extends Command
      */
     protected $signature = 'user:test 
         {--user= : The user name (optional)}
-        {--tags= : Comma separated list of tags (optional)}
         {--skip-tags= : Comma separated list of skipped tags (optional)}
         {--nova-batch-id= : The nova batch id (optional)}
         {--disable-tty : Disable TTY}';
@@ -49,47 +44,49 @@ class UserTestCommand extends Command
     {
         $this->askUser();
 
-        if ($this->host && $this->user) {
-            $user = $this->userModel;
-            $cmd = ['ansible-playbook', '-i', $this->getInventoryScript(), $this->getProvisionPlaybook()];
-
-            $extraVars = "host=$this->host test_user=$this->user";
-            $cmd = array_merge($cmd, ["--extra-vars", $extraVars]);
-
-            $tags = "test-user";
-            if ($this->option('tags')) {
-                $tags .= "," . $this->option('tags');
+        $apps = [];
+        $domains = [];
+        foreach ($this->userModel->apps as $app) {
+            $apps[] = $app->getVar('name');
+            $domains[] = $app->getVar('domain');
+            $aliases = $app->getVar('aliases', '', []);
+            foreach ($aliases as $alias) {
+                $domains[] = $alias;
             }
-            $cmd = array_merge($cmd, ["--tags", $tags]);
-
-            if ($this->option('skip-tags')) {
-                $cmd = array_merge($cmd, ["--skip-tags", $this->option('skip-tags')]);
-            }
-
-            $process = new Process($cmd);
-            $process->setTty($this->getTTY());
-            $process->setTimeout(3600);
-
-            $process->run(function ($type, $buffer) use ($user) {
-                if (Process::ERR === $type || preg_match("/failed=[1-9]\d*/", $buffer) || preg_match("/unreachable=[1-9]\d*/", $buffer)) {
-                    self::addToProcessBuffer($buffer);
-                    Notification::route('slack', env('SLACK_HOOK'))
-                        ->notify(new CommandFailed("Failed to test user.", self::getProcessBuffer()));
-                    throw new Exception("Failed to test the user!\n" . self::getProcessBuffer());
-                } else {
-                    self::addToProcessBuffer($buffer);
-                }
-            });
-
-            if ($this->option('nova-batch-id')) {
-                $event = Nova::actionEvent();
-                $event::where('batch_id', $this->option('nova-batch-id'))
-                    ->where('model_type', $user->getMorphClass())
-                    ->where('model_id', $user->getKey())
-                    ->update(['exception' => self::getProcessBuffer()]);
-            }
-        } else {
-            throw new Exception("Could not find user.");
         }
+
+        $databases = [];
+        foreach ($this->userModel->databases as $database) {
+            $databases[] = $database->getVar('name');
+        }
+
+        $authKeys = [];
+        foreach ($this->userModel->authKeys as $key) {
+            $authKeys[] = $key->getVar('key');
+        }
+
+        $vars = [
+            "host" => $this->host,
+            "user" => $this->user,
+            "mysql_password" => $this->userModel->getVar('mysql_password'),
+            "isolated" => $this->userModel->getVar('isolated'),
+            "apps" => $apps,
+            "auth_keys" => $authKeys,
+            "databases" => $databases,
+            "domains" => $domains
+        ];
+
+        $validations = [
+            'host' => 'required|exists:server_hosts,name,state,' . HasState::getProvisionedIndex(),
+            'user' => 'required|exists:server_users,name,state,' . HasState::getProvisionedIndex(),
+            'mysql_password' => 'required|min:6',
+            'isolated' => 'required|boolean',
+            'apps' => 'array',
+            'auth_keys' => 'array',
+            'databases' => 'array',
+            'domains' => 'array',
+        ];
+
+        $this->runPlaybook($this->userModel, 'user/test.yml', $vars, $validations, "Failed to test user.");
     }
 }
