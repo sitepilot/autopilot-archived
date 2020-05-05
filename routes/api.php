bf1d38c4c73c90e6c37ddba391984c962f39e286
Nick Jansen <n.jansen@sitepilot.io>
2020-05-05 10:22:13 +0200

    Add API routes

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,36 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+Route::middleware('auth:sanctum')->get('/v1/user', function (Request $request) {
     return $request->user();
 });
+
+// Jobs
+Route::middleware('auth:sanctum')->get('/v1/jobs/{job}', 'JobController@show');
+
+// Server Groups
+Route::middleware('auth:sanctum')->apiResource('/v1/groups', 'ServerGroupController');
+
+// Server Hosts
+Route::middleware('auth:sanctum')->apiResource('/v1/hosts', 'ServerHostController');
+Route::middleware('auth:sanctum')->post('/v1/hosts/{host}/provision', 'ServerHostController@provision');
+Route::middleware('auth:sanctum')->post('/v1/hosts/{host}/test', 'ServerHostController@test');
+Route::middleware('auth:sanctum')->post('/v1/hosts/{host}/cert-renew', 'ServerHostController@certRenew');
+
+// Server Users
+Route::middleware('auth:sanctum')->apiResource('/v1/users', 'ServerUserController');
+Route::middleware('auth:sanctum')->post('/v1/users/{user}/test', 'ServerUserController@test');
+
+// Server Apps
+Route::middleware('auth:sanctum')->apiResource('/v1/apps', 'ServerAppController');
+Route::middleware('auth:sanctum')->post('/v1/apps/{app}/cert-request', 'ServerAppController@certRequest');
+
+// Server Apps - WordPress
+Route::middleware('auth:sanctum')->post('/v1/apps/{app}/wp/check-state', 'ServerAppController@wpCheckState');
+Route::middleware('auth:sanctum')->post('/v1/apps/{app}/wp/install', 'ServerAppController@wpInstall');
+Route::middleware('auth:sanctum')->post('/v1/apps/{app}/wp/login', 'ServerAppController@wpLogin');
+Route::middleware('auth:sanctum')->post('/v1/apps/{app}/wp/search-replace', 'ServerAppController@wpSearchReplace');
+Route::middleware('auth:sanctum')->post('/v1/apps/{app}/wp/update', 'ServerAppController@wpUpdate');
+
+// Server Databases
+Route::middleware('auth:sanctum')->apiResource('/v1/databases', 'ServerDatabaseController');
