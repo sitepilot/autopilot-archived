e66cdc5dfdb7f19011e528e9fdb06c782302b4b2
Nick Jansen <n.jansen@sitepilot.io>
2020-05-05 10:18:36 +0200

    Extend controller functions

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,12 +2,68 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Support\Arr;
+use Illuminate\Http\Request;
+use Illuminate\Database\Eloquent\Model;
+use Imtigger\LaravelJobStatus\JobStatus;
+use App\Http\Resources\JobStatusResource;
 use Illuminate\Foundation\Bus\DispatchesJobs;
-use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    /**
+     * Update config of a model.
+     *
+     * @param Request $request
+     * @param Model $model
+     * @return Model
+     */
+    public function updateConfig(Request $request, Model $model)
+    {
+        $defaultVars = $model->getDefaultVars();
+
+        // Disable name and hostname update
+        unset($defaultVars['name']);
+        unset($defaultVars['hostname']);
+
+        foreach ($request->input('config', []) as $key => $value) {
+            if (Arr::has($defaultVars, $key)) {
+                $model->setVar($key, $value);
+            }
+        }
+
+        return $model;
+    }
+
+    /**
+     * Dispatches a job by name.
+     * 
+     * @param string $class
+     * @param Request $request
+     * @param Model $host
+     * @return Response
+     */
+    public function dispatchJob($class, $resourceClass, Request $request, Model $model, $status = 200)
+    {
+        $job = new $class($model, $request);
+        $this->dispatch($job);
+
+        if ($request->input('dispatch') == 'now') {
+            while (JobStatus::find($job->getJobStatusId())->finished_at == null) {
+                sleep(1);
+            }
+        }
+
+        return (new JobStatusResource(
+            JobStatus::find($job->getJobStatusId()),
+            new $resourceClass($model),
+        ))
+            ->response()
+            ->setStatusCode($status);
+    }
 }
